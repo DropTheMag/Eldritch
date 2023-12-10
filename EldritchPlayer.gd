77ab737cf992cd0d332f21f8538a33e1292a8f0a
DropTheMag <46230134+DropTheMag@users.noreply.github.com>
2023-12-10 14:48:05 -0500

    move

---
+++ EldritchPlayer.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var speed = 125
+
+func _process(delta):
+	var direction = Vector2()
+	
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+
+	position += direction.normalized() * speed * delta
