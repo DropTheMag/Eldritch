77734b7d54ad79971a8f1342536fa63e79252c90
DropTheMag <46230134+DropTheMag@users.noreply.github.com>
2023-12-11 15:02:41 -0500

    Y-Sorting Setup and Fixed, Collision Boxes Redesigned, Scripts Cleaned Up

--- PlayerBody.gd
+++ PlayerBody.gd
@@ -1,35 +1,19 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+const SPEED = 150.0
 
 func _physics_process(delta):
 	
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var Horizontaldirection = Input.get_axis("ui_left", "ui_right")
-	var Verticaldirection = Input.get_axis("ui_up", "ui_down")
-	if Horizontaldirection:
-		velocity.x = Horizontaldirection * SPEED
+	var HDirection = Input.get_axis("ui_left", "ui_right")
+	var VDirection = Input.get_axis("ui_up", "ui_down")
+	
+	if HDirection:
+		velocity.x = HDirection * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if Verticaldirection:
-		velocity.y = Verticaldirection * SPEED
+	if VDirection:
+		velocity.y = VDirection * SPEED
 	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
-
-
-func _on_area_2d_area_entered(area):
-	pass # Replace with function body.
