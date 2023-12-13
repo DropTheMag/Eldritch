de512e17974d611de8d22c1689405bc61ad223e6
Omaerta29 <153414344+Omaerta29@users.noreply.github.com>
2023-12-13 13:35:29 -0500

    Starting to Work on Health UI

--- Globals/GlobalVariables.gd
+++ Globals/GlobalVariables.gd
@@ -3,6 +3,12 @@ extends Node
 # Global variables for use in the entire program
 # Used externally by prefixing with GV.(variable name here)
 
+#Dialogue System
 var dialog_open = false
 # Set how long the dialog stays open when the character leaves a dialog initiator
 var dialog_timer = 2
+
+#Health Bar
+var health = 100:
+	input.
+
---
+++ User Interface/ui.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+@onready var health: TextureProgressBar = $MarginContainer/HealthBar
+ 
+func update_health_bar():
+	
+	
