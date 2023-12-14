16c8536ebbc61385b8e521338f6ad2eb20fa4456
DropTheMag <46230134+DropTheMag@users.noreply.github.com>
2023-12-14 17:55:33 -0500

    Cleaned Up Sprint Mechanics

--- Player/PlayerController.gd
+++ Player/PlayerController.gd
@@ -3,14 +3,13 @@ extends CharacterBody2D
 var default_speed = 100
 var sprint_factor = 1.5
 var can_move = true
-var can_sprint = true
 
 var max_stamina = 100
 var current_stamina = max_stamina
 var stamina_depletion_rate = 0.5
 var stamina_regeneration_rate = 0.25
 var regen_delay = 2.5  # in seconds
-var sprint_stopped_time = 0.0
+var regen_delay_timer = 0.0
 
 @onready var player_sprite = $Sprite
 
@@ -20,17 +19,16 @@ func _physics_process(delta):
 	# Disable player movement if required for specific events (death, dialogue, cutscene, etc.)
 	if can_move:
 		# Player sprint controller
-		if can_sprint and Input.is_action_pressed("shift"):
+		if Input.is_action_pressed("shift"):
 			if current_stamina > 0:
 				current_stamina -= stamina_depletion_rate
 				speed = default_speed * sprint_factor
-				sprint_stopped_time = 0.0
+				regen_delay_timer = 0.0
 			else:
 				speed = default_speed
 		else:
-			sprint_stopped_time += delta
-
-			if sprint_stopped_time >= regen_delay:
+			regen_delay_timer += delta
+			if regen_delay_timer >= regen_delay:
 				current_stamina += stamina_regeneration_rate
 				current_stamina = clamp(current_stamina, 0, max_stamina)
 				speed = default_speed
