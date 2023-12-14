cf06b5b5bf9c6b48549f159ab5b9d9452e258bcc
DropTheMag <46230134+DropTheMag@users.noreply.github.com>
2023-12-14 11:40:50 -0500

    Instant Spike Damage and Code Cleanup
    - Added instant damage to spikes on collision, then timed damage.
    - Cleaned up object references and improved readability.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,33 +1,35 @@
 extends CharacterBody2D
 
-var HEALTH = 100
-var SPEED = 100
+var health = 100
+var speed = 100
 var can_move = true
+
+var movement_vector = Vector2()
 
 var dialogue_initiator_collider = null
 
 func _ready():
 	# Set player initial health value
-	$PlayerUI/HealthBar.value = HEALTH
+	$PlayerUI/HealthBar.value = health
 
 func _physics_process(delta):
 	
 	# Player controller
 	if can_move:
-		var HDirection = Input.get_axis("ui_left", "ui_right") * SPEED
-		var VDirection = Input.get_axis("ui_up", "ui_down") * SPEED
+		movement_vector.x = Input.get_axis("ui_left", "ui_right") * speed
+		movement_vector.y = Input.get_axis("ui_up", "ui_down") * speed
 		# Change the sprites direction when going left or right
-		if HDirection < 0:
-			get_node("Character").scale.x = abs(get_node("Character").scale.x)
-		if HDirection > 0:
-			get_node("Character").scale.x = -abs(get_node("Character").scale.x)
+		if movement_vector.x < 0:
+			$Character.scale.x = abs($Character.scale.x)
+		elif movement_vector.x > 0:
+			$Character.scale.x = -abs($Character.scale.x)
 	
 		# Move player and grab collider info
-		var collision_info = move_and_collide(Vector2(HDirection, VDirection) * delta)
+		var collision_info = move_and_collide(movement_vector * delta)
 	
 	# Set health bar, monitor player health and perform functions on it.
-	$PlayerUI/HealthBar.value = HEALTH
-	if HEALTH <=0:
+	$PlayerUI/HealthBar.value = health
+	if health <=0:
 		can_move = false
 		$RespawnButton.visible = true
 	
@@ -45,21 +47,19 @@ func _physics_process(delta):
 func _on_respawn_button_pressed():
 	$RespawnButton.visible = false
 	position = Vector2(get_parent().position.x - 128, get_parent().position.y - 128)
-	HEALTH = 100
+	health = 100
 	can_move = true
 	
 # Check if player enters dialog initiator, if so, show/set dialog and reset previous timer (if running)
 func _on_dialog_collision_tracker_body_entered(body):
 	dialogue_initiator_collider = body
-	var DialogueButton = $DialogueUI/DialogueButton
 	if dialogue_initiator_collider.is_in_group("HelperBot"):
-		DialogueButton.reset_dialogue_timer()
-		DialogueButton.show_random_dialogue(dialogue_initiator_collider.dialogue_sequence)
+		$DialogueUI/DialogueButton.reset_dialogue_timer()
+		$DialogueUI/DialogueButton.show_random_dialogue(dialogue_initiator_collider.dialogue_sequence)
 
 # Check if the player leaves a dialog initiator, if so, start dialog closing timer
 func _on_dialog_collision_tracker_body_exited(body):
-	var DialogueButton = $DialogueUI/DialogueButton
 	if dialogue_initiator_collider:
 		if dialogue_initiator_collider.is_in_group("HelperBot"):
-			DialogueButton.reset_dialogue_timer(true)
+			$DialogueUI/DialogueButton.reset_dialogue_timer(true)
 		dialogue_initiator_collider = null
--- Traps/GroundSpikes.gd
+++ Traps/GroundSpikes.gd
@@ -2,21 +2,27 @@ extends Area2D
 
 var time_between_hits = 0.5
 var player_in_spikes = false
+var hit_at_start = true
 var player:CharacterBody2D = null
 
 func _process(delta):
 	if player_in_spikes:
+		if not hit_at_start:
+			player.health -= 20
+			hit_at_start = true
 		if time_between_hits > 0:
 			time_between_hits -= delta
 		else:
 			if player.is_in_group("Player"):
 				time_between_hits = 0.5
-				player.HEALTH -=20
+				player.health -=20
 			
 func _on_body_entered(body):
 	player = body
+	hit_at_start = false
 	player_in_spikes = true
 
 func _on_body_exited(body):
 	player = null
+	hit_at_start = true
 	player_in_spikes = false
