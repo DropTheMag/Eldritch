09693d989e3a0a51578fcd3a18f4b34b63db46fd
Omaerta29 <153414344+Omaerta29@users.noreply.github.com>
2023-12-21 22:54:45 -0500

    Merging Into Main

---
+++ NPCs/AnonymousNPC/AnotherNPC.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+
+var dialogue_sequence = ["Well hello there, Chayse.", 
+"You seem to have stumbled upon somthing you shouldn't have seen.", 
+"I think you should turn around and forget what you have seen.", 
+"Fuck off. Please."]
